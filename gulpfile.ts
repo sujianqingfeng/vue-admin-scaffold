f66ac2e413d7fe17240b0fbd16511e7689ea8d74
hens <121116111@qq.com>
2022-10-15 23:17:31 +0800

    chore: build js

--- gulpfile.ts
+++ gulpfile.ts
@@ -2,9 +2,56 @@ import { src, parallel, dest } from 'gulp'
 import path from 'path'
 import gulpSass from 'gulp-sass'
 import dartSass from 'sass'
+import type { Plugin, OutputOptions, RollupBuild  } from 'rollup'
+import { rollup } from 'rollup'
+import vue from '@vitejs/plugin-vue'
+import glob from 'fast-glob'
+import esbuild from 'rollup-plugin-esbuild'
+import commonjs from '@rollup/plugin-commonjs'
 
-const scssPath =  path.resolve(__dirname, './src/style.scss')
-const libFolder = path.resolve(__dirname, 'lib')
+const resolve = (...paths: string[]) => path.resolve(__dirname, ...paths)
+
+const scssPath =  resolve('./src/style.scss')
+const libFolder = resolve('dist')
+
+function writeBundles(bundle: RollupBuild, options: OutputOptions[]) {
+  return Promise.all(options.map((option) => bundle.write(option)))
+}
+
+async function buildModules() {
+  const input = await glob('**/*.{js,ts,vue}', {
+    cwd: resolve('src'),
+    absolute: true,
+    onlyFiles: true
+  })
+
+  const bundle = await rollup({
+    input,
+    plugins: [
+      vue({
+        isProduction: false
+      }) as Plugin,
+      esbuild({
+        loaders: {
+          '.vue': 'ts'
+        }
+      }),
+      commonjs()
+    ],
+    external: ['vue', '@sujian/utils', 'deepmerge']
+  })
+
+  await writeBundles(bundle, [
+    {
+      format: 'esm',
+      dir: resolve(libFolder, 'es')
+    },
+    {
+      format: 'commonjs',
+      dir: resolve(libFolder, 'lib')
+    }
+  ])
+}
 
 function buildStyle() {
   const sass = gulpSass(dartSass)
@@ -17,6 +64,6 @@ function copyScss() {
   return src(scssPath).pipe(dest(libFolder))
 }
 
-const build = parallel(buildStyle, copyScss)
+const build = parallel(buildModules, buildStyle, copyScss)
 
 export default build
